73d98013719ae5c5a73c3d612cfa2338dba508af
MarcosGR19 <marcos.ace98@gmail.com>
2023-01-19 18:34:51 +0100

    change home path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'home', component:HomeComponent},
+  {path:'', component:HomeComponent},
   {path:'animes', component:AnimesComponent}
 ];
 
